c0ee716c50f4f9f793119029119d494ad4651f9c
Chris Watson <chris@watzon.me>
2018-01-27 03:35:45 -0700

    Update the readme and bump the version

--- src/garlicoin.cr
+++ src/garlicoin.cr
@@ -2,13 +2,35 @@ require "./core_extensions/*"
 require "./garlicoin/models/*"
 require "./garlicoin/*"
 
-# TODO: Write documentation for `Garlicoin`
+# This is a wrapper for the Garlicoin daemon's JSON-RPC API. All of the commands that are
+# avaialble via the `garlicoin-cli` utility are also available here, in snake_cased_form.
+# This includes commands such as `get_wallet_info` and `send_to_address`. Commands that
+# return a large JSON object will typically map said object to a class using
+# `JSON.mapping`. This makes it a lot easier to navigate them than with a typical
+# `JSON::Any` object, as well as leaving the door open for future improvements.
+#
+# ## Example use
+#
+# ```crystal
+# require "garlicoin"
+
+# # Connect to garlicoind using the information supplied in your garlicoin.conf file
+# client = Garlicoin::Client.new("localhost", 42070, "username", "password")
+
+# accounts = client.list_accounts
+# pp accounts # => { "": 2.53575342 }
+
+# # Or you can lead your config from your garlicoin.conf directly
+# conf = Garlicoin::Config.load
+# client = Garlicoin::Client.new(conf)
+
+# accounts = client.list_accounts
+# pp accounts # => { "": 2.53575342 }
+# ```
 module Garlicoin
-  # TODO: Put your code here
+
+  def self.version
+    return VERSION
+  end
+
 end
-
-config = Garlicoin::Config.load
-client = Garlicoin::Client.new(config)
-
-tx = client.list_wallets
-pp tx
